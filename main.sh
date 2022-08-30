1cfd021591bc5013aa5312cfacb78cf9bf950187
ney roger <52115064+nperea75-gh@users.noreply.github.com>
2022-08-30 18:14:04 +0000

    se realiza adicion de la linea 12

--- main.sh
+++ main.sh
@@ -5,3 +5,7 @@ echo nada que decir
 echo quiero aprender esto por eso sigo dandole
 echo aca estoy en el branche development
 echo se hace esto para enlazar con el github repo replit
+
+
+
+echo **************************
